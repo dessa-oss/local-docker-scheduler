73c9a9be462bb84e9d03f0a4593a1b17a0c509c6
ekhl <el@deeplearni.ng>
2019-09-07 13:11:03 -0400

    Added atexit to shutdown scheduler without waiting

--- local_docker_scheduler/__init__.py
+++ local_docker_scheduler/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_apscheduler import APScheduler
+import atexit
 
 app = Flask(__name__)
 
@@ -9,6 +10,7 @@ scheduler = APScheduler()
 # it is also possible to enable the API directly
 # scheduler.api_enabled = True
 scheduler.init_app(app)
+atexit.register(lambda: scheduler.shutdown(wait=False))
 
 import docker_worker_pool
 docker_worker_pool.add()
