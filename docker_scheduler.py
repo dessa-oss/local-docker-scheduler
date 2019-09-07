1af48448fba2488fd2461aa49dd734d4224ad4d4
ekhl <el@deeplearni.ng>
2019-09-07 15:22:09 -0400

    cleaned up README in plugins removed extra files and updated .gitignore

--- docker_scheduler.py
+++
@@ -1,7 +0,0 @@
-from app import app
-import logging
-
-if __name__ == '__main__':
-    logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.DEBUG)
-
-    app.run(use_reloader=False, host='0.0.0.0')
