cfa5e51e2c9f66a72f86bedba006a83cf4697abb
ekhl <el@deeplearni.ng>
2019-09-09 02:57:32 -0400

    added redis implementation to DB

--- db/__init__.py
+++ db/__init__.py
@@ -1,8 +1,13 @@
 from db.repositionable_queue import Queue
 from db.sqlite_dict_connection import connect
+from .redis_connection import RedisDict, RedisList
 
+# failed_jobs = connect('./failed_jobs.sqlite')
+# completed_jobs = connect('./completed_jobs.sqlite')
+# running_jobs = connect('./running_jobs.sqlite')
+# queue = Queue()
 
-failed_jobs = connect('./failed_jobs.sqlite')
-completed_jobs = connect('./completed_jobs.sqlite')
-running_jobs = connect('./running_jobs.sqlite')
-queue = Queue()
+failed_jobs = RedisDict("failed_jobs", "127.0.0.1", "6379")
+completed_jobs = RedisDict("completed_jobs", "127.0.0.1", "6379")
+running_jobs = RedisDict("running_jobs", "127.0.0.1", "6379")
+queue = RedisList("queue", "127.0.0.1", "6379")
---
+++ db/redis_connection.py
@@ -0,0 +1,72 @@
+import redis
+from pickle import loads, dumps
+
+
+class RedisDict:
+    def __init__(self, key, host, port, db=0):
+        self._redis = redis.StrictRedis(host, port, db)
+        self._key = key
+
+    def __getitem__(self, field):
+        return loads(self._redis.hget(self._key, dumps(field)))
+
+    def __setitem__(self, field, value):
+        return self._redis.hset(self._key, dumps(field), dumps(value))
+
+    def items(self):
+        return [(loads(field), loads(value)) for field, value in self._redis.hgetall(self._key).items()]
+
+    def keys(self):
+        return [loads(field) for field in self._redis.hkeys(self._key)]
+
+    def __delitem__(self, field):
+        return self._redis.hdel(self._key, field)
+
+
+class RedisList:
+    def __init__(self, key, host, port, db=0):
+        self._redis = redis.StrictRedis(host, port, db)
+        self._key = key
+
+    def __getitem__(self, index):
+        response = self._redis.lindex(self._key, index)
+        if response is None:
+            raise IndexError
+        return loads(response)
+
+    def __setitem__(self, index, value):
+        return self._redis.lset(self._key, index, dumps(value))
+
+    def __delitem__(self, index):
+        return self._redis.lrem(self._key, 1, self[index])
+
+    def insert(self, index, value):
+        return self._redis.linsert(self._key, "before", self[index], dumps(value))
+
+    def append(self, value):
+        return self._redis.rpush(self._key, dumps(value))
+
+    def pop(self, index=-1):
+        if index == -1:
+            response = self._redis.rpop(self._key)
+            if response is None:
+                raise IndexError
+            return loads(response)
+        elif index == 0:
+            response = self._redis.lpop(self._key)
+            if response is None:
+                raise IndexError
+            return loads(response)
+        else:
+            response = self[index]
+            del self[index]
+            return response
+
+    def reposition(self, original_position, new_position):
+        temp = self[original_position]
+        del self[original_position]
+        try:
+            self.insert(new_position, temp)
+        except IndexError:
+            self.insert(original_position, temp)
+            raise
