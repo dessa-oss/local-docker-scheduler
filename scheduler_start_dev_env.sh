87188b6f869f90ab190f8d7a2d170f68b8e15ba0
Katherine <k.bancroft@dessa.com>
2019-10-22 11:47:12 -0400

    update the script for running in environment

--- scheduler_start_dev_env.sh
+++ scheduler_start_dev_env.sh
@@ -12,6 +12,11 @@ then
     exit 1
 fi
 
+if [ "$1" = "rebuild" ]; then
+    docker build -t local-docker-scheduler:latest .
+fi
+
+
 canonical_home=$(cd ${FOUNDATIONS_HOME} && pwd)
 
 cat > database.config.yaml << EOF
@@ -47,13 +52,54 @@ redis_tracker_client:
   port: 6379
 EOF
 
+
 archives_dir=${canonical_home}/job_data
 working_dir=${canonical_home}/config/local_docker_scheduler/work_dir
 
 mkdir -p ${archives_dir} ${working_dir}
 
+create_network_if_not_exists () {
+    if [[ -z "$(docker network ls --filter name=foundations-orbit --format \"{{.ID}}\")" ]]; then
+        echo 'Creating foundations-orbit docker network'
+        docker network create -d bridge foundations-orbit
+    fi
+}
+
+get_redis_container_image () {
+    docker ps --format "{{.Image}}" | grep -E '^redis:?.*$' | head -n1
+}
+
+create_redis_if_not_exists () {
+    if [[ -z "$(get_redis_container_image)" ]]; then
+        echo "Creating redis."
+        docker run -d \
+            --restart always \
+            --name foundations-redis \
+            -p 6379:6379 \
+            redis:5 \
+            > /dev/null
+        echo 'Redis continer successfully started and attached to the network'
+        if [[ $? -ne 0 ]]; then
+            echo "Failed to start redis."
+            exit 1
+        fi
+    fi
+}
+
+create_network_if_not_exists
+create_redis_if_not_exists
+
+redis_container_image=$(get_redis_container_image)
+redis_container_name=$(docker ps -f ancestor=${redis_container_image} --format "{{.Names}}" | head -n1)
+
+echo 'Attemtping to connect the redis server $redis_container_name to the network foundations-orbit'
+docker network connect foundations-orbit ${redis_container_name} --alias redis >> /dev/null
+
+echo 'Attempting to create docker for local-docker-scheduler'
 docker run --rm -d --name local-docker-scheduler \
     -p 5000:5000 \
+    -e NUM_WORKERS=0 \
+    --network foundations-orbit \
     -v ${archives_dir}:/archives \
     -v ${working_dir}:/working_dir \
     -v /var/run/docker.sock:/var/run/docker.sock \
@@ -61,3 +107,8 @@ docker run --rm -d --name local-docker-scheduler \
     -v $(pwd -P)/tracker_client_plugins.yaml:/app/local-docker-scheduler/tracker_client_plugins.yaml \
     -v $(pwd -P)/database.config.yaml:/app/local-docker-scheduler/database.config.yaml \
     local-docker-scheduler
+
+if [[ $? -ne 0 ]]; then
+    echo "Failed to start local-docker-scheduler."
+    exit 1
+fi
